78bd378a64de2db2921be84483395cc616625735
Alexandersebastiandreyer <alexander.dreyer@dvag.de>
2025-08-29 19:47:18 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // WICHTIG: Repo-Name eintragen (genau wie das GitHub-Repo heißen wird)
-  base: "/REPO_NAME/",
+  base: "/Kundenportal/",  // genau wie dein Repo-Name!
 });
